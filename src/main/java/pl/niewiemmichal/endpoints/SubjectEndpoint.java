59d8dd04f0ef899c382f2e65246a6355bd6a1dfb
Michał Granda <majcoch98@gmail.com>
2019-04-10 21:40:17 +0200

    Create Subject Endpoint with respect to changes enforced by @Sh1ftry

--- src/main/java/pl/niewiemmichal/endpoints/SubjectEndpoint.java
+++ src/main/java/pl/niewiemmichal/endpoints/SubjectEndpoint.java
@@ -1,11 +1,18 @@
 package pl.niewiemmichal.endpoints;
 
+import pl.niewiemmichal.commons.exceptions.ResourceDoesNotExistException;
 import pl.niewiemmichal.model.Subject;
 import pl.niewiemmichal.repository.Repository;
 
 import javax.inject.Inject;
+import javax.validation.Valid;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
 
+@Path("/subjects")
 public class SubjectEndpoint {
+
     private Repository<Subject, Long> subjectRepository;
 
     @Inject
@@ -14,4 +21,40 @@ public class SubjectEndpoint {
     }
 
     public SubjectEndpoint() {}
+
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Subject getSubject(@PathParam("id") Long id){
+        return subjectRepository.findById(id).orElseThrow(() -> new ResourceDoesNotExistException("Subject", "id", id.toString()));
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Subject> getAllSubjects() {
+        return subjectRepository.findAll();
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Subject addSubject(@Valid Subject subject) {
+        return subjectRepository.save(subject);
+    }
+
+    @PUT
+    @Path("/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Subject updateSubject(@PathParam("id") Long id, @Valid Subject subject) {
+        if(subjectRepository.findById(id).isPresent()) return subjectRepository.update(subject);
+        else return subjectRepository.save(subject);
+    }
+
+    @DELETE
+    @Path("/{id}")
+    public void deleteSubject(@PathParam("id") Long id) {
+        subjectRepository.findById(id)
+                .ifPresent(q -> subjectRepository.delete(q));
+    }
 }
