7ed3abbf151e709252d82cc3f64e0be0843fc692
Michał Padula <m.padula@onet.pl>
2019-04-12 18:30:23 +0200

    Replace database

--- src/main/java/pl/niewiemmichal/config/DataSourceConfig.java
+++ src/main/java/pl/niewiemmichal/config/DataSourceConfig.java
@@ -6,7 +6,7 @@ import javax.ejb.Startup;
 @DataSourceDefinition(
         className = "com.mysql.cj.jdbc.MysqlDataSource",
         name = "java:comp/env/MySQLDataSource",
-        url="jdbc:mysql://database:3306/database",
+        url="jdbc:mysql://wtyt-database.ct8carsorhgh.eu-central-1.rds.amazonaws.com:3306/wtyt_database",
         user = "root",
         password = "password"
 )
